4751a0fc576e4ad178d1a5c3839b7634b88b890b
tommyrachmadiono <tommyrachmadiono@gmail.com>
2022-01-01 11:26:04 +0700

    Remove unused component on top nav

--- resources/views/templates/partials/top_nav.blade.php
+++ resources/views/templates/partials/top_nav.blade.php
@@ -7,67 +7,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Dashboard</a>
+            <a class="navbar-brand" href="{{ route('dashboard.index') }}">
+                Dashboard {{ auth()->user()->role->jenis_role }}
+            </a>
         </div>
         <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-left">
-                <li>
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-dashboard"></i>
-                        <p class="hidden-lg hidden-md">Dashboard</p>
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-globe"></i>
-                        <b class="caret hidden-lg hidden-md"></b>
-                        <p class="hidden-lg hidden-md">
-                            5 Notifications
-                            <b class="caret"></b>
-                        </p>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Notification 1</a></li>
-                        <li><a href="#">Notification 2</a></li>
-                        <li><a href="#">Notification 3</a></li>
-                        <li><a href="#">Notification 4</a></li>
-                        <li><a href="#">Another notification</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-search"></i>
-                        <p class="hidden-lg hidden-md">Search</p>
-                    </a>
-                </li>
-            </ul>
 
             <ul class="nav navbar-nav navbar-right">
                 <li>
-                    <a href="">
-                        <p>Account</p>
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <p>
-                            Dropdown
-                            <b class="caret"></b>
-                        </p>
-
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('form-logout').submit();">
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('form-logout').submit();">
                         <p>Log out</p>
                     </a>
                     <form id="form-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
